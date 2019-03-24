b8bffc92624e64b3cc19e689065caaf8427fc040
nulltemp <nulltemp999@gmail.com>
2019-03-24 23:37:17 +0900

    天気予報の取得

--- handler.js
+++ handler.js
@@ -1,14 +1,15 @@
-'use strict';
+"use strict";
 
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const axios = require("axios");
+const config = require("config");
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+module.exports.hello = async () => {
+  const response = await axios.get(
+    `https://api.openweathermap.org/data/2.5/forecast?zip=${config.zip},${
+      config.countryCode
+    }&APPID=${config.apiKey}`
+  );
+  console.log(
+    `status=${response.status}, data=${JSON.stringify(response.data)}`
+  );
 };
